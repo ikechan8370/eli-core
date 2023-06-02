49e90988926c457fa47972d09b9920b6c947f30d
ikechan8370 <geyinchibuaa@gmail.com>
2023-06-02 21:30:09 +0800

    fix: add export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import {OpenAIClient} from "./clients/openai"
 import {ChatGPTClient} from "./clients/chatgpt";
+import {SydneyLLMClient} from "./clients/sydney";
+import {SlackClaudeClient} from "./clients/slackClient";
+import {XinghuoClient} from "./clients/xh";
+
 
 export default {
     OpenAIClient,
-    ChatGPTClient
+    ChatGPTClient,
+    SydneyLLMClient,
+    SlackClaudeClient,
+    XinghuoClient
 }
 
